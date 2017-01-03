2822eaf3174259e1eb455d3d489b3cab82b00eb8
Isaac Samuel <getintouchwithisaac@gmail.com>
2017-01-03 13:49:58 -0500

    basic add/read funtionality

--- post_entry.php
+++ post_entry.php
@@ -4,12 +4,10 @@ error_reporting(E_ALL);
 ini_set('display_errors', true);
 	$number = 1;
 	$file_name = (string) $number . ".txt";
-	echo $file_name;
 
 	while (file_exists($file_name) == true) {
 		$number++;
 		$file_name = (string) $number . ".txt";
-		echo "blah";
 	}
 
 	$file = fopen($file_name, "w");
@@ -17,15 +15,16 @@ ini_set('display_errors', true);
         echo ( "Error in opening file" );
         exit();
      }
-	fwrite($file, "<tags>");
-	fwrite($file, $_POST["tags"]);
-	fwrite($file, "</tags>");
-	fwrite($file, "<br>");
+	fwrite($file, "<div class = 'post'>");
+	fwrite($file, "<div class = 'post-content'>");
 	fwrite($file, $_POST["post"]);
-	flock($file, LOCK_UN);
+	fwrite($file, "</div");
+	fwrite($file, "<br>");
+	fwrite($file, "<div class = 'tags'>");
+	fwrite($file, $_POST["tags"]);
+	fwrite($file, "</div>");
+	fwrite($file, "</div>");
 	fclose($file);
-	echo $file_name;
-	echo $_POST["post"];
 	header("Location: show_entries.php");
 	exit;
 ?>
--- show_entries.php
+++ show_entries.php
@@ -10,15 +10,13 @@
 			$number = 1;
 			$file_name = (string) $number . ".txt";
 
-			while (file_exists($file_name) == true) {
-				$file = fopen($file_name, "w");
-				echo fread($file, filesize(file_name));
+			while (file_exists($file_name)) {
+				$file = fopen($file_name, "r");
+				echo fread($file, filesize($file_name));
 				fclose($file);
+				$number++;
+				$file_name = (string) $number . ".txt";
 			}
-
-			$number++;
-			$file_name = (string) $number . ".txt";
-			echo "blah";
 
 			?>
 	</div>
